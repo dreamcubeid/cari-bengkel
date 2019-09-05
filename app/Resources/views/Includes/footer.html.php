b4c06a884bde1c4401b354094a2fe1359ea32785
Falmesino Abdul Hamid <falmesino@gmail.com>
2019-09-05 13:58:50 +0700

    WIP Landing page

--- app/Resources/views/Includes/footer.html.php
+++ app/Resources/views/Includes/footer.html.php
@@ -2,14 +2,18 @@
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-8 col-lg-9">
-                <a href="#" title="">
-                    <img src="http://placehold.it/745x150?text=Google+AdSense" alt="Google AdSense" class="img-fluid" style="min-width: 100%; height: auto;">
-                </a>
+                <div class="cn-ads my-2 my-md-0">
+                    <a href="#" title="">
+                        <img data-src="http://placehold.it/745x150?text=Google+AdSense" alt="Google AdSense" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
+                    </a>
+                </div><!--/ .cn-ads -->
             </div><!--/ .col-12 -->
             <div class="col-12 col-md-4 col-lg-3">
-                <a href="#" title="">
-                    <img src="http://placehold.it/235x150?text=Google+AdSense" alt="Google AdSense" class="img-fluid" style="min-width: 100%; height: auto;">
-                </a>
+                <div class="cn-ads my-2 my-md-0">
+                    <a href="#" title="">
+                        <img data-src="http://placehold.it/235x150?text=Google+AdSense" alt="Google AdSense" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
+                    </a>
+                </div><!--/ .cn-ads -->
             </div><!--/ .col-12 -->
         </div><!--/ .row -->
     </div><!--/ .container -->
@@ -20,7 +24,7 @@
         <div class="row d-flex align-items-center">
             <div class="col-12 col-md-4">
                 <a href="#">
-                    <img src="http://placehold.it/120x64?text=Cari.in" alt="Cari.in" class="img-fluid">
+                    <img data-src="http://placehold.it/120x64?text=Cari.in" alt="Cari.in" class="img-fluid img-lazy">
                 </a>
             </div><!--/ .col-12 -->
             <div class="col-12 col-md-8 text-right">
--- app/Resources/views/Layouts/layout.html.php
+++ app/Resources/views/Layouts/layout.html.php
@@ -5,15 +5,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Exo+2:400,600&display=swap"> 
+        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Exo+2:300,400,600&display=swap"> 
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.min.css">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+        <!--
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.min.css">
+-->
 
         <?php 
             // Put your css file in web/static/css/
             $this->headLink()->prependStylesheet('/static/css/global.css');
+            $this->headLink()->prependStylesheet('/static/css/style.css');
 
             // Put your css plugins fuels in web/static/lib/
             $this->headLink()->appendStylesheet('/static/lib/example-plugin/css/plugin.css');
@@ -33,6 +36,7 @@
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/js/all.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.10/jquery.lazy.min.js"></script>
 
         <?php 
             // Put your js file in web/static/js/
