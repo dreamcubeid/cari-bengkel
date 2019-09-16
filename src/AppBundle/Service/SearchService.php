c1fbea39972bb3724acab63360b8ea51ee7c2047
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2019-09-16 17:17:56 +0700

    Search service for search nearby garage

--- src/AppBundle/Service/SearchService.php
+++ src/AppBundle/Service/SearchService.php
@@ -2,7 +2,27 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Contract\GarageRepositoryInterface;
+
 class SearchService
 {
-    
+    private $garageRepo;
+
+    public function __construct(GarageRepositoryInterface $garageRepo)
+    {
+        $this->garageRepo = $garageRepo;
+    }
+
+    public function findBy(array $condition = [], array $location = [], string $orderBy = 'o_creationDate', string $sortBy = 'DESC')
+    {
+        if (!empty($location)) 
+        {
+            $orderBy = 'Distance';
+            $sortBy = 'ASC';
+        }
+        
+        $result = $this->garageRepo->findBy($condition, $location, $orderBy, $sortBy);
+
+        return $result;
+    }
 }
