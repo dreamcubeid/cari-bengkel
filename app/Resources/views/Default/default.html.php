11d5980b347401bae387f2442c6ac29c8b9451f7
Hasmya Dwi Azra <hasmyazra@gmail.com>
2019-09-19 18:12:21 +0700

    Show empty garage information if no nearest garage

--- app/Resources/views/Default/default.html.php
+++ app/Resources/views/Default/default.html.php
@@ -78,7 +78,7 @@
             </div><!--/ .col-12 -->
         </div><!--/ .row -->
 
-        <div class="row py-5">
+        <div class="row py-5" id="info-empty-garage" hidden="true">
             <div class="col-12 text-center">
 
                 <h2 style="font-weight: 300; color: #414141;" class="m-0 p-0">
@@ -307,136 +307,140 @@
             data : data,
             success:function(response) {
 
-                $.each(response.data, function(key, value) {
-                    var ele = '';
-                    
-                    ele += '<div class="cn-card-slider__item">';
+                if (response.data.length) {
+                    $.each(response.data, function(key, value) {
+                        var ele = '';
+                        
+                        ele += '<div class="cn-card-slider__item">';
 
-                        ele += '<div class="cn-card mb-5 mb-lg-0">';
-                            
-                            ele += '<div class="cn-card-header">';
-                                ele += '<div class="cn-card-tags text-right">';
-                                    ele += '<ul class="list-inline m-0 p-0">';
+                            ele += '<div class="cn-card mb-5 mb-lg-0">';
+                                
+                                ele += '<div class="cn-card-header">';
+                                    ele += '<div class="cn-card-tags text-right">';
+                                        ele += '<ul class="list-inline m-0 p-0">';
 
-                                    if (value.GarageTypeName.includes("Mobil")) {
-                                        ele += '<li class="list-inline-item">';
-                                            ele += '<span class="cn-card-tags__item">';
-                                                ele += '<i class="fa fa-car-side"></i>';
+                                        if (value.GarageTypeName.includes("Mobil")) {
+                                            ele += '<li class="list-inline-item">';
+                                                ele += '<span class="cn-card-tags__item">';
+                                                    ele += '<i class="fa fa-car-side"></i>';
+                                                ele += '</span>';
+                                            ele += '</li>';
+                                        }
+
+                                        if (value.GarageTypeName.includes("Motor")) {
+                                            ele += '<li class="list-inline-item">';
+                                                ele += '<span class="cn-card-tags__item">';
+                                                    ele += '<i class="fa fa-motorcycle"></i>';
+                                                ele += '</span>';
+                                            ele += '</li>';
+                                        }                                 
+
+                                        ele += '</ul>';
+                                    ele += '</div>';
+
+                                    ele += '<div class="cn-card-image">';
+                                        ele += '<img data-src="' + (value.BannerPath ? value.BannerPath : '/static/images/default/default-banner.png') + '" class="card-img-top img-lazy" alt="">';
+                                    ele += '</div><!--/ .cn-card-image -->';
+
+                                    ele += '<div class="cn-card-avatar">';
+                                        ele += '<a href="#" title="' + value.Name + '">';
+                                            ele += '<img data-src="' + (value.LogoPath ? value.LogoPath : '/static/images/default/default-image.png') + '" alt="' + value.Name + '" class="img-lazy">';
+                                        ele += '</a>';
+                                    ele += '</div><!--/ .cn-card-avatar -->';
+                                ele += '</div><!--/ .cn-card-header -->';
+
+                                ele += '<div class="cn-card-body">';
+                                    ele += '<h5 class="cn-card-title m-0 mb-3 p-0">';
+                                        ele += '<a href="#" title=' + value.Name + '">';
+                                            ele += value.Name;
+                                        ele += '</a>';
+                                    ele += '</h5>';
+                                    ele += '<ul class="cn-card-info m-0 p-0 list-unstyled">';
+                                        ele += '<li>';
+                                            ele += '<span>';
+                                                ele += '<i class="fa fa-map-marker-alt"></i>';
                                             ele += '</span>';
+                                            ele += '<span>' + value.Address.concat(', ', value.CityStatus,' ', value.City) + '</span>';
                                         ele += '</li>';
-                                    }
-
-                                    if (value.GarageTypeName.includes("Motor")) {
-                                        ele += '<li class="list-inline-item">';
-                                            ele += '<span class="cn-card-tags__item">';
-                                                ele += '<i class="fa fa-motorcycle"></i>';
+                                        
+                                        ele += '<li>';
+                                            ele += '<span>';
+                                                ele += '<i class="far fa-clock"></i>';
                                             ele += '</span>';
+                                            ele += '<span>Jadwal Buka</span>';
+                                            ele += '<div class="cn-card-popup">';
+                                                ele += '<a href="#" title="Klik untuk melihat jadwal" class="ml-auto mr-0">';
+                                                    ele += '<i class="fas fa-chevron-down"></i>';
+                                                ele += '</a>';
+                                                ele += '<div class="cn-card-popup__body py-2 px-3 ml-auto mr-0 mt-1">';
+
+                                                    if (value.OperatingHours.length > 0) {
+                                                        $.each(value.OperatingHours, function(keyOp, valOp) {
+                                                            ele += '<p class="m-0 p-0 d-flex flex-row align-items-center justify-content-start">';
+                                                                ele += '<span class="align-self-start flex-grow-1">' + valOp.OperationalDay + '</span>';
+                                                                ele += '<span class="align-self-end text-right">' + valOp.OpenHour + ' - ' + valOp.CloseHour + ' WIB</span>';
+                                                            ele += '</p>';
+                                                        });
+                                                    } else {
+                                                        ele += '<p class="m-0 p-0 d-flex flex-row align-items-center justify-content-start">';
+                                                            ele += '<span class="align-self-start flex-grow-1">Jadwal Belum Tersedia</span>';
+                                                            ele += '<span class="align-self-end text-right"></span>';
+                                                        ele += '</p>';
+                                                    }
+
+                                                ele += '</div><!--/ .cn-card-popup__body -->';
+                                            ele += '</div><!--/ .cn-card-popup -->';
                                         ele += '</li>';
-                                    }                                 
 
                                     ele += '</ul>';
-                                ele += '</div>';
+                                    ele += '<div class="text-center mt-5">';
+                                        ele += '<a href="#" class="btn btn-cn-primary btn-cn--bold">Selengkapnya</a>';
+                                    ele += '</div>';
+                                ele += '</div><!--/ .card-body -->';
+                            ele += '</div><!--/ .cn-card -->';
 
-                                ele += '<div class="cn-card-image">';
-                                    ele += '<img data-src="' + (value.BannerPath ? value.BannerPath : '/static/images/default/default-banner.png') + '" class="card-img-top img-lazy" alt="">';
-                                ele += '</div><!--/ .cn-card-image -->';
+                        ele += '</div><!--/ .cn-card-slider__item -->';
 
-                                ele += '<div class="cn-card-avatar">';
-                                    ele += '<a href="#" title="' + value.Name + '">';
-                                        ele += '<img data-src="' + (value.LogoPath ? value.LogoPath : '/static/images/default/default-image.png') + '" alt="' + value.Name + '" class="img-lazy">';
-                                    ele += '</a>';
-                                ele += '</div><!--/ .cn-card-avatar -->';
-                            ele += '</div><!--/ .cn-card-header -->';
+                        $('#nearest-garage-list').append(ele);
 
-                            ele += '<div class="cn-card-body">';
-                                ele += '<h5 class="cn-card-title m-0 mb-3 p-0">';
-                                    ele += '<a href="#" title=' + value.Name + '">';
-                                        ele += value.Name;
-                                    ele += '</a>';
-                                ele += '</h5>';
-                                ele += '<ul class="cn-card-info m-0 p-0 list-unstyled">';
-                                    ele += '<li>';
-                                        ele += '<span>';
-                                            ele += '<i class="fa fa-map-marker-alt"></i>';
-                                        ele += '</span>';
-                                        ele += '<span>' + value.Address.concat(', ', value.CityStatus,' ', value.City) + '</span>';
-                                    ele += '</li>';
-                                    
-                                    ele += '<li>';
-                                        ele += '<span>';
-                                            ele += '<i class="far fa-clock"></i>';
-                                        ele += '</span>';
-                                        ele += '<span>Jadwal Buka</span>';
-                                        ele += '<div class="cn-card-popup">';
-                                            ele += '<a href="#" title="Klik untuk melihat jadwal" class="ml-auto mr-0">';
-                                                ele += '<i class="fas fa-chevron-down"></i>';
-                                            ele += '</a>';
-                                            ele += '<div class="cn-card-popup__body py-2 px-3 ml-auto mr-0 mt-1">';
-
-                                                if (value.OperatingHours.length > 0) {
-                                                    $.each(value.OperatingHours, function(keyOp, valOp) {
-                                                        ele += '<p class="m-0 p-0 d-flex flex-row align-items-center justify-content-start">';
-                                                            ele += '<span class="align-self-start flex-grow-1">' + valOp.OperationalDay + '</span>';
-                                                            ele += '<span class="align-self-end text-right">' + valOp.OpenHour + ' - ' + valOp.CloseHour + ' WIB</span>';
-                                                        ele += '</p>';
-                                                    });
-                                                } else {
-                                                    ele += '<p class="m-0 p-0 d-flex flex-row align-items-center justify-content-start">';
-                                                        ele += '<span class="align-self-start flex-grow-1">Jadwal Belum Tersedia</span>';
-                                                        ele += '<span class="align-self-end text-right"></span>';
-                                                    ele += '</p>';
-                                                }
-
-                                            ele += '</div><!--/ .cn-card-popup__body -->';
-                                        ele += '</div><!--/ .cn-card-popup -->';
-                                    ele += '</li>';
-
-                                ele += '</ul>';
-                                ele += '<div class="text-center mt-5">';
-                                    ele += '<a href="#" class="btn btn-cn-primary btn-cn--bold">Selengkapnya</a>';
-                                ele += '</div>';
-                            ele += '</div><!--/ .card-body -->';
-                        ele += '</div><!--/ .cn-card -->';
-
-                    ele += '</div><!--/ .cn-card-slider__item -->';
-
-                    $('#nearest-garage-list').append(ele);
-
-                });
-
-                if($('.cn-card-slider__items').length > 0) {
-                    $('.cn-card-slider__items').slick({
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
-                        dots: true,
-                        arrows: true,
-                        infinite: false,
-                        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
-                        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
-                        appendArrows: $('.cn-card-slider__control'),
-                        appendDots: $('.cn-card-slider__control'),
-                        responsive: [
-                            {
-                                breakpoint: 991,
-                                settings: {
-                                    slidesToShow: 2,
-                                    slidesToScroll: 2
-                                }
-                            },
-                            {
-                                breakpoint: 767,
-                                settings: {
-                                    slidesToShow: 1,
-                                    slidesToScroll: 1
-                                }
-                            }
-                        ]
                     });
-                }
 
-                $('.img-lazy').Lazy({
-                    // Options
-                });
+                    if($('.cn-card-slider__items').length > 0) {
+                        $('.cn-card-slider__items').slick({
+                            slidesToShow: 3,
+                            slidesToScroll: 3,
+                            dots: true,
+                            arrows: true,
+                            infinite: false,
+                            prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+                            nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+                            appendArrows: $('.cn-card-slider__control'),
+                            appendDots: $('.cn-card-slider__control'),
+                            responsive: [
+                                {
+                                    breakpoint: 991,
+                                    settings: {
+                                        slidesToShow: 2,
+                                        slidesToScroll: 2
+                                    }
+                                },
+                                {
+                                    breakpoint: 767,
+                                    settings: {
+                                        slidesToShow: 1,
+                                        slidesToScroll: 1
+                                    }
+                                }
+                            ]
+                        });
+                    }
+
+                    $('.img-lazy').Lazy({
+                        // Options
+                    });
+                } else {
+                    $("#info-empty-garage").removeAttr('hidden');
+                }
 
             },
             error:function(response) {
