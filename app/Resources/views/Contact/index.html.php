b798465e65fc1ed4e3e997cff2d005207fb32913
Falmesino Abdul Hamid <falmesino@gmail.com>
2019-10-14 03:54:14 +0700

    WIP Contact Page

---
+++ app/Resources/views/Contact/index.html.php
@@ -0,0 +1,92 @@
+<?php
+
+    $this->extend('Layouts/layout.html.php');
+
+?>
+
+<section class="my-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+
+                <ol class="breadcrumb cn-breadcrumb px-0">
+                    <li class="breadcrumb-item">
+                        <a href="/">Home</a>
+                    </li>
+                    <li class="breadcrumb-item active">
+                        <span>Contact Us</span>
+                    </li>
+                </ol>
+
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
+</section><!--/ . -->
+
+<section class="mb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-8 col-lg-6 pb-5">
+                <div class="cn-hero">
+                    <h1>Contact Us</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua.</p>
+                </div><!--/ .cn-hero -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
+</section><!--/ . -->
+
+<section class="mb-5 pb-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-5">
+
+                <form role="form">
+                    <div class="form-group">
+                        <input type="text" class="form-control" tabindex="1" required placeholder="Nama Lengkap">
+                    </div><!--/ .form-group -->
+                    <div class="form-group">
+                        <input type="email" class="form-control" tabindex="2" required placeholder="Email">
+                    </div><!--/ .form-group -->
+                    <div class="form-group">
+                        <input type="tel" class="form-control" tabindex="3" required placeholder="Nomor Telepon">
+                    </div><!--/ .form-group -->
+                    <div class="form-group">
+                        <textarea class="form-control" tabindex="4" required placeholder="Tuliskan pesan anda" rows="5"></textarea>
+                    </div><!--/ .form-group -->
+                    <div class="form-group">
+                        <p class="my-5 p-0 text-center">
+                            Taro captcha di marih
+                        </p>
+                    </div><!--/ .form-group -->
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-cn-primary btn-cn--bold" tabindex="5">
+                            Kirim Pesan
+                        </button>
+                    </div><!--/ .form-group -->
+                </form>
+
+            </div><!--/ .col-12 -->
+            <div class="col-12 col-lg-5 offset-lg-2">
+                <div class="cn-section pt-0 mt-5">
+                    <div class="cn-ads my-2 my-md-0">
+                        <a href="#">
+                            <img data-src="http://placehold.it/320x250?text=Google+Ads" alt="Google Ads" title="Google Ads" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
+                        </a>
+                    </div>
+                </div><!--/ .cn-section -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+        <div class="row">
+            <div class="col-12 d-flex flex-row align-items-end">
+                <div class="cn-section pt-0 mt-5">
+                    <div class="cn-ads my-2 my-md-0">
+                        <a href="#">
+                            <img data-src="http://placehold.it/830x80?text=Google+Ads" alt="Google Ads" title="Google Ads" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
+                        </a>
+                    </div>
+                </div><!--/ .cn-section -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
+</section><!--/ . -->
