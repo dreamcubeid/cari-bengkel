538bb6dae9797d73c026bdcec7559a1e3309b1a0
Falmesino Abdul Hamid <falmesino@gmail.com>
2019-11-04 07:01:27 +0700

    Update footer

--- app/Resources/views/Includes/footer.html.php
+++ app/Resources/views/Includes/footer.html.php
@@ -19,34 +19,91 @@
     <!-- </div>/ .container  -->
 <!-- </section>/ .  -->
 
-<footer class="cn-footer d-flex flex-column align-items-center justify-content-center mt-auto mb-0 align-self-end" style="min-width: 100%;">
-    <div class="container">
-        <div class="row d-flex align-items-center">
-            <div class="col-12 col-md-4 text-center text-md-left">
-                <a href="#" class="d-block mb-1 mb-md-0 text-center text-md-left">
-                    <img data-src="static/images/logo-footer.png" alt="Kemudi" class="img-fluid img-lazy mx-auto mx-md-0">
-                </a>
-            </div><!--/ .col-12 -->
-            <div class="col-12 col-md-8 text-center text-md-right">
-                <ul class="list-inline mb-2 p-0">
-                    <li class="list-inline-item">
-                        <a href="#" target="_blank">
-                            <i class="fas fa-envelope-square"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" target="_blank">
-                            <i class="fab fa-facebook-square"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#" target="_blank">
-                            <i class="fab fa-twitter-square"></i>
-                        </a>
-                    </li>
-                </ul>
-                <p class="mb-3 mb-md-0 m-0 p-0"><small>&copy; Copyright 2019</small></p>
-            </div><!--/ .col-12 -->
-        </div><!--/ .row -->
-    </div><!--/ .container -->
+<footer class="cn-footer">
+    <section class="cn-footer__top">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-5 text-center text-md-left">
+                    <a href="#" class="d-block mb-5 mb-lg-0 text-center text-md-left">
+                        <img data-src="static/images/logo-footer.png" alt="Kemudi" class="img-fluid img-lazy mx-auto mx-md-0">
+                    </a>
+                </div><!--/ .col-12 -->
+                <div class="col-12 col-md-4 col-lg-2 text-center text-md-left">
+                    <div class="cn-footer__item mb-5 mb-md-0">
+                        <h4 class="cn-footer__item__title">Links</h4>
+                        <ul class="list-unstyled">
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                        </ul>
+                    </div><!--/ .cn-footer__item -->
+                </div><!--/ .col-12 -->
+                <div class="col-12 col-md-4 col-lg-2 text-center text-md-left">
+                    <div class="cn-footer__item mb-5 mb-md-0">
+                        <h4 class="cn-footer__item__title">Links</h4>
+                        <ul class="list-unstyled">
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                            <li class="">
+                                <a href="#" title="">Sample Link Item</a>
+                            </li>
+                        </ul>
+                    </div><!--/ .cn-footer__item -->
+                </div><!--/ .col-12 -->
+                <div class="col-12 col-md-4 col-lg-3 text-center text-md-left">
+                    <div class="cn-footer__item mb-0 mb-md-0">
+                        <h4 class="cn-footer__item__title">Address</h4>
+                        <p>Jl. Bukit Cimanggu City Raya, RT02/RW13, Cibadak, Kec. Tanah Sareal, Kota Bogor, Jawa Barat 16166</p>
+                    </div><!--/ .cn-footer__item -->
+                </div><!--/ .col-12 -->
+            </div><!--/ .row -->
+        </div><!--/ .container -->
+    </section>
+    <section class="cn-footer__bottom d-flex flex-row align-items-center justify-content-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 text-center text-md-left">
+                    <p class="mb-3 mb-md-0 m-0 p-0"><small>&copy; Copyright 2019</small></p>
+                </div><!--/ .col-12 -->
+                <div class="col-12 col-md-6 text-center text-md-right">
+                    <ul class="list-inline m-0 p-0">
+                        <li class="list-inline-item">
+                            <a href="#" target="_blank">
+                                <i class="fas fa-envelope-square"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" target="_blank">
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" target="_blank">
+                                <i class="fab fa-twitter-square"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div><!--/ .col-12 -->
+            </div><!--/ .row -->
+        </div><!--/ .container -->
+    </section>
 </footer>
