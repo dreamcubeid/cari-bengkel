d1b51c05bfeea3eb69b0a19f929b9f4f6428972a
Falmesino Abdul Hamid <falmesino@gmail.com>
2019-11-04 07:19:12 +0700

    Add contact page success icon

--- app/Resources/views/Contact/index.html.php
+++ app/Resources/views/Contact/index.html.php
@@ -1,14 +1,9 @@
-<?php
-
-$this->extend('Layouts/layout.html.php');
-
-?>
+<?php $this->extend('Layouts/layout.html.php'); ?>
 
 <section class="my-4">
     <div class="container">
         <div class="row">
             <div class="col-12">
-
                 <ol class="breadcrumb cn-breadcrumb px-0">
                     <li class="breadcrumb-item">
                         <a href="/">Home</a>
@@ -17,15 +12,10 @@ $this->extend('Layouts/layout.html.php');
                         <span>Contact Us</span>
                     </li>
                 </ol>
-
-            </div>
-            <!--/ .col-12 -->
-        </div>
-        <!--/ .row -->
-    </div>
-    <!--/ .container -->
-</section>
-<!--/ . -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
+</section><!--/ . -->
 
 <section class="mb-5">
     <div class="container">
@@ -34,65 +24,78 @@ $this->extend('Layouts/layout.html.php');
                 <div class="cn-hero">
                     <h1>Contact Us</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnaaliqua.</p>
-                </div>
-                <!--/ .cn-hero -->
-            </div>
-            <!--/ .col-12 -->
-        </div>
-        <!--/ .row -->
-    </div>
-    <!--/ .container -->
+                </div><!--/ .cn-hero -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
 </section>
-<!--/ . -->
+
+<section class="my-5 mx-auto">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-8 offset-md-2 text-center">
+
+                <h2 class="m-0 p-0">Terimakasih telah mengirimkan pesan kepada kami, silahkan cek email Anda untuk mengetahui langkah selanjutnya.</h2>
+
+                <div class="row my-5">
+                    <div class="col-8 offset-2 col-md-6 offset-md-3">
+                        <img src="/static/images/mail-logo.png" alt="" class="img-fluid d-block mx-auto">
+                    </div><!--/ .col-12 -->
+                </div><!--/ .row -->
+
+                <a href="#" class="btn btn-cn-primary btn-cn--bold mb-5">
+                    Home
+                </a>
+
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+    </div><!--/ .container -->
+</section>
 
 <section class="mb-5 pb-5">
     <div class="container">
+
         <div class="row">
+
             <div class="col-12 col-lg-5">
 
                 <form id="contactUs" role="form" class="cn-form">
                     <div class="form-group">
                         <input id="name" type="text" class="form-control" tabindex="1" required placeholder="Nama Lengkap">
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                     <div class="form-group">
                         <input id="email" type="email" class="form-control" tabindex="2" required placeholder="Email">
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                     <div class="form-group">
                         <input id="phone" type="tel" class="form-control" tabindex="3" required placeholder="Nomor Telepon">
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                     <div class="form-group">
                         <textarea id="message" class="form-control" tabindex="4" required placeholder="Tuliskan pesan anda" rows="5"></textarea>
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                     <div class="form-group">
                         <p class="my-5 p-0 text-center">
                             Taro captcha di marih
                         </p>
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                     <div class="form-group mb-5 mb-lg-0 text-center text-lg-left">
                         <button id="send" type="submit" class="btn btn-cn-primary btn-cn--bold" tabindex="5">
                             Kirim Pesan
                         </button>
-                    </div>
-                    <!--/ .form-group -->
+                    </div><!--/ .form-group -->
                 </form>
 
-            </div>
-            <!--/ .col-12 -->
+            </div><!--/ .col-12 -->
+
             <div class="col-12 col-lg-5 offset-lg-2 d-flex flex-row align-items-end">
                 <div class="cn-ads p-0 mx-auto my-2 my-lg-0 mr-lg-0 ml-lg-auto">
                     <a href="#">
                         <img data-src="http://placehold.it/320x250?text=Google+Ads" alt="Google Ads" title="Google Ads" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
                     </a>
                 </div>
-            </div>
-            <!--/ .col-12 -->
-        </div>
-        <!--/ .row -->
+            </div><!--/ .col-12 -->
+
+        </div><!--/ .row -->
+
         <div class="row">
             <div class="col-12">
                 <div class="cn-section py-0 mt-5">
@@ -101,16 +104,12 @@ $this->extend('Layouts/layout.html.php');
                             <img data-src="http://placehold.it/1000x150?text=Google+Ads" alt="Google Ads" title="Google Ads" class="img-fluid img-lazy" style="min-width: 100%; height: auto;">
                         </a>
                     </div>
-                </div>
-                <!--/ .cn-section -->
-            </div>
-            <!--/ .col-12 -->
-        </div>
-        <!--/ .row -->
-    </div>
-    <!--/ .container -->
+                </div><!--/ .cn-section -->
+            </div><!--/ .col-12 -->
+        </div><!--/ .row -->
+
+    </div><!--/ .container -->
 </section>
-<!--/ . -->
 
 <script>
     $("#send").click(function(e) {
