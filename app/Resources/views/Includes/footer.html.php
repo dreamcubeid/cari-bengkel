29c6605cfe91b44894d8bba9756ed56260980d8b
Falmesino Abdul Hamid <falmesino@gmail.com>
2019-11-04 07:19:40 +0700

    Fix footer logo URL

--- app/Resources/views/Includes/footer.html.php
+++ app/Resources/views/Includes/footer.html.php
@@ -25,7 +25,7 @@
             <div class="row">
                 <div class="col-12 col-lg-5 text-center text-md-left">
                     <a href="#" class="d-block mb-5 mb-lg-0 text-center text-md-left">
-                        <img data-src="static/images/logo-footer.png" alt="Kemudi" class="img-fluid img-lazy mx-auto mx-md-0">
+                        <img data-src="/static/images/logo-footer.png" alt="Kemudi" class="img-fluid img-lazy mx-auto mx-md-0">
                     </a>
                 </div><!--/ .col-12 -->
                 <div class="col-12 col-md-4 col-lg-2 text-center text-md-left">
