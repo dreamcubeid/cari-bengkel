27f00ca60249bf665e21dcae8903defb48b63f98
Hasmya Dwi Azra <hasmyazra@gmail.com>
2019-11-12 16:24:39 +0700

    Add helper method to get site url

--- src/AppBundle/Utils/Url.php
+++ src/AppBundle/Utils/Url.php
@@ -25,4 +25,29 @@ class Url {
         return $string;
     } 
 
+    public static function getSiteUrl(string $authentication = '', bool $getPath = false, bool $withParams = true) 
+    {
+        $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
+        $host =  htmlentities($_SERVER['HTTP_HOST']);        
+
+        $result = $protocol . $host;
+
+        if ($getPath) {
+            $requestUri = htmlentities($_SERVER['REQUEST_URI']);
+            $result .= $requestUri;
+
+            if(!$withParams){
+                $result = strtok($result, "?");                
+            }
+        } else { 
+            $result .= '/'; 
+        }
+
+        if ($authentication) {
+            $result = $authentication . "@" . $result;
+        }
+
+        return $result;
+    }  
+
 }
